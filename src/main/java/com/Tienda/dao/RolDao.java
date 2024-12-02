954955032c68408402186b0f97f639f0fc3d059e
xbyro <xbyro@Byron>
2024-12-01 21:04:12 -0600

    Finalizacion semana 11

---
+++ src/main/java/com/Tienda/dao/RolDao.java
@@ -0,0 +1,11 @@
+
+package com.Tienda.dao;
+
+import com.Tienda.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
+
